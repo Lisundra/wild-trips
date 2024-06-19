aa8e88ac84d7798a2ea9aad10454dd2dca25e77c
pustuleczek <pustuleczek@outlook.com>
2024-06-19 20:37:21 +0200

    commit before merge

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,6 +17,7 @@ import { useAppDispatch, useAppSelector } from './redux/hooks';
 import { decrement, increment } from './redux/postSlice';
 import { fetchPosts } from './redux/thunkActions';
 import Navbar from './components/Navbar/Navbar';
+import CardDetails from './Pages/CardDetails/CardDetails';
 // import { fetchPosts } from './redux/thunkActions';
 // import { PostsType } from './types';
 
@@ -85,6 +86,7 @@ function App(): JSX.Element {
     }>
 
             </Route>
+            <Route path="card-details" element={<CardDetails />} />
       </Routes>
       </div>
 
