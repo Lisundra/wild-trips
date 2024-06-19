9cc4b6e4b39361206080ed85186af4d5195d60db
pustuleczek <pustuleczek@outlook.com>
2024-06-19 22:15:42 +0200

    update DB associations

--- server/src/routers/api.router.js
+++ server/src/routers/api.router.js
@@ -1,5 +1,6 @@
 const postRouter = require('./post.api.router');
 const apiRouter = require('express').Router();
+const toursRouter = require('express').Router();
 
 apiRouter.use('/posts', postRouter);
 apiRouter.use('/tours', toursRouter);
