24453257bcc6c31b3866ad4b0d2dad533217b8e4
Lisundra <dariasokolovagalkina@gmail.com>
2024-06-20 02:04:52 +0200

    work on nodemailer

--- client/src/components/EmailSubscription/EmailSubscription.tsx
+++ client/src/components/EmailSubscription/EmailSubscription.tsx
@@ -1,15 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './EmailSubscription.module.css';
 import { Button, Input } from 'antd';
+import axios from 'axios';
 
 export default function EmailSubscription() {
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Логика для отправки email
+    console.log('Отправить запрос с email:', email);
+  };
+
+
+  const sendMail = () => {
+    axios
+      .get("http://localhost:5000/", {
+        params: {
+          email,
+          subject,
+          message,
+        },
+      })
+      .then(() => {
+        //success
+        console.log("success");
+      })
+      .catch(() => {
+        console.log("failure");
+      });
+  };
+
   return (
     <div className={styles.subscriptionContainer}>
       <h3 className='text-4xl font-bold text-gray-800'>Делимся лучшим</h3>
       <p className='text-lg'>Подпишитесь на рассылку, чтобы увидеть новые туры первым!</p>
       <div className="signUpContainer flex items-center mt-4">
-        <form action="" method='post' id='signUpForm' target='_blank' className="flex">
-          <Input name="email" placeholder="Введите ваш email" className="" required/>
+        <form onSubmit={handleSubmit} className="flex">
+          <Input
+            name="email"
+            type='text'
+            placeholder="Введите ваш email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            required
+          />
+          {/* <p>{email}</p> */}
           <Button htmlType="submit" className="bg-emerald-500 hover:bg-emerald-400 text-white">Подписаться</Button>
         </form>
       </div>
--- server/src/controllers/subscribe-controller.js
+++ server/src/controllers/subscribe-controller.js
@@ -1,31 +1,31 @@
 const nodemailer = require('nodemailer');
 
-async function sendEmail({ email, subject, message }) {
-  return new Promise((resolve, reject) => {
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+// async function sendEmail({ email, subject, message }) {
+//   return new Promise((resolve, reject) => {
+//     const transporter = nodemailer.createTransport({
+//       service: 'gmail',
+//       auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS,
+//       },
+//     });
 
-    const mailConfigs = {
-      from: process.env.EMAIL_USER,
-      to: email,
-      subject: subject,
-      html: `<p>${message}</p><p>Best Regards</p>`,
-    };
+//     const mailConfigs = {
+//       from: process.env.EMAIL_USER,
+//       to: email,
+//       subject: subject,
+//       html: `<p>${message}</p><p>Best Regards</p>`,
+//     };
 
-    transporter.sendMail(mailConfigs, (error, info) => {
-      if (error) {
-        console.log(error);
-        return reject({ message: 'An error has occurred' });
-      }
-      return resolve({ message: 'Email sent successfully' });
-    });
-  });
-}
+//     transporter.sendMail(mailConfigs, (error, info) => {
+//       if (error) {
+//         console.log(error);
+//         return reject({ message: 'An error has occurred' });
+//       }
+//       return resolve({ message: 'Email sent successfully' });
+//     });
+//   });
+// }
 
 module.exports = {
   sendSubscriptionConfirmation: async (req, res) => {
@@ -41,4 +41,10 @@ module.exports = {
       res.status(400).json({ err: err.message });
     }
   },
+
+
+//   sendSubscriptionConfirmation: (req, res) => {
+//     res.send('Hello!');
+//     console.log('-________________-');
+//   },
 }; 
